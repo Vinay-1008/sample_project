b087d401b685e809126cd9b09f8798679cb0fffd
Vinay <vinayreddy100800@gmail.com>
2024-10-16 19:11:40 +0530

    Uncommented in test get API file

--- test_get_API.py
+++ test_get_API.py
@@ -1,9 +1,6 @@
 
-# def test_add_item():
-#     print("Item added successfully")
-#
-# def test_remove_item():
-#     print("Item removed successfully")
-#
-# def test_calculate():
-#     assert 10+1 == 11
+def test_add_item():
+    print("Item added successfully")
+
+def test_remove_item():
+    print("Item removed successfully")
